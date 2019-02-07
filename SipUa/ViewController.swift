81c833d411156ad809fe46e101bceb911347f55f
NLDeviOS <censpi@gmail.com>
2019-02-07 14:28:43 +0700

    Initial Commit

---
+++ SipUa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SipUa
+//
+//  Created by NLDeviOS on 7/2/2562 BE.
+//  Copyright © 2562 NLDeviOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
